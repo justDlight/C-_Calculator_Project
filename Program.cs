cadcb9f9180e60f10c1b9b13e6aee22ce51648bf
John <John.weiss1996@gmail.com>
2023-03-22 17:07:16 +1100

    Added some basic addition functions.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,40 @@ namespace Calculator_Project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello this is a test");
+
+            adding_two_nums();
+            adding_users_nums();
+            Console.ReadLine();
+        }
+
+        static void adding_two_nums()
+        {
+            int number_4 = 234;
+            int number_5 = 549;
+            int number_6 = 0;
+
+            number_6 = number_4 + number_5;
+
+            Console.WriteLine(number_6);
+        }
+
+        static void adding_users_nums() 
+        {
+            int final_num = 0;
+
+            Console.WriteLine("Enter your first number:");
+            int usersNum_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number:");
+            int usersNum_2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your first number is: " + usersNum_1 + " & your second number is: " + usersNum_2);
+            Console.WriteLine("CALCULATING ADDING NUMBERS TOGETHER CALCULATING ADDITION");
+            final_num = usersNum_1 + usersNum_2;
+            Console.WriteLine("CALCULATING COMPLETE FINAL NUMBER IS: " + final_num);
+
+
         }
     }
 }

28caf840ca39756a083883715e522b0e769be835
John <John.weiss1996@gmail.com>
2023-03-24 13:25:14 +1100

    Added a Calculator Logic Library Class and some basic addition methods subtraction multiplication and division.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Calculator_Logic;
 
 namespace Calculator
 {
@@ -6,8 +7,25 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            float x = 2;
+            float y = 3;
+
             Console.WriteLine("Hello, World!");
+
+            //Class1.addition_return(x, y); //This does nothing since your just getting 500 and not displaying it. Fixed below 
+            Console.WriteLine(Class1.addition_return(x, y));//Now you are displaying the value 5 since x + y = 5
+            Console.WriteLine(Class1.subtraction_return(x, y));
+            Console.WriteLine(Class1.division_return(x, y));
+            Console.WriteLine(Class1.multiplication_return(x, y));
+
+            //ERROR STARTS HERE!!
+            //Type casting a float into an int.
+            //Trying to turn a float into an int find more details on why in the Class1 file will change that name to dont worry.
+            Console.WriteLine(Class1.to_the_power_of_return((int)x, (int)y));//type cast because of ^ operator NOT WORKING PROPERLY
+
+
             Console.WriteLine(Assembly.GetExecutingAssembly().Location);
         }
+        
     }
 }
---
+++ Calculator_Logic/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator_Logic
+{
+    public class Class1
+    {
+        //The way we will need to use the methods returning the value of 5 here
+        public static float addition_return(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float subtraction_return(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float division_return(float x, float y)
+        {
+            return x / y;
+        }
+
+        public static float multiplication_return(float x, float y)
+        {
+            return x * y;
+        }
+
+        //Reason that I need these as ints is because the ^ Operator does not work on floats only ints.
+        //So I have them as ints here
+        public static int to_the_power_of_return(int x, int y)
+        {
+            return x ^ y;
+        }
+    }
+}
